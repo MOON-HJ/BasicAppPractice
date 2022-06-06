0043337ffe191b234e4c282b8b9b6a95ed950a5b
Moon Hyo Jae <followmoonrise@gmail.com>
2022-06-06 16:04:19 +0900

    feat : 날씨 화면 Label, Image, Button UI 구성

--- Weather/Sources/Weather/WeatherViewController.swift
+++ Weather/Sources/Weather/WeatherViewController.swift
@@ -10,10 +10,42 @@ import SnapKit
 import Then
 
 public final class WeatherViewController: UIViewController {
-  private let textLabel = UILabel().then {
-    $0.text = "Hello World"
+  private let stackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.alignment = .center
+    $0.spacing = 10
+  }
+ 
+  private let cityLabel = UILabel().then {
+    $0.text = "Seoul"
     $0.textColor = .label
-    $0.sizeToFit()
+    $0.textAlignment = .center
+    $0.font = .systemFont(ofSize: 34, weight: .medium)
+  }
+  
+  private let imageView = UIImageView().then {
+    $0.image = UIImage(systemName: "sun.max.fill")?
+      .withRenderingMode(.alwaysOriginal)
+    $0.contentMode = .scaleAspectFill
+  }
+  
+  private let degree = UILabel().then {
+    $0.text = "27°"
+    $0.textColor = .label
+    $0.textAlignment = .center
+    $0.font = .systemFont(ofSize: 70, weight: .medium)
+  }
+  
+  private let weeklyWeather = UIStackView().then {
+    $0.axis = .horizontal
+  }
+  
+  private let changeCityButton = UIButton().then {
+    $0.setTitle("Change City", for: .normal)
+    $0.setTitleColor(.systemTeal, for: .normal)
+    $0.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+    $0.backgroundColor = .white
+    $0.layer.cornerRadius = 40
   }
   
   public init() {
@@ -31,17 +63,24 @@ public final class WeatherViewController: UIViewController {
   }
   
   func configureUI() {
-    view.backgroundColor = .systemBackground
+    view.backgroundColor = .systemTeal
     
-    [textLabel].forEach {
-      view.addSubview($0)
-    }
-    
+    view.addSubview([stackView])
+    stackView.addArrangedSubview([cityLabel, imageView, degree, weeklyWeather, changeCityButton])
   }
   
   func configureConstraints() {
-    textLabel.snp.makeConstraints {
-      $0.center.equalToSuperview()
+    stackView.snp.makeConstraints {
+      $0.edges.equalTo(view.safeAreaLayoutGuide).inset(UIEdgeInsets(top: 30, left: 20, bottom: 20, right: 30))
+    }
+    
+    imageView.snp.makeConstraints {
+      $0.height.equalTo(180)
+    }
+    
+    changeCityButton.snp.makeConstraints {
+      $0.height.equalTo(80)
+      $0.width.equalToSuperview()
     }
   }
 }
